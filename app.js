60a148bfb478b68bfee3ace55e987d2b74b7aa33
Jeremy Kent <ibiwan@gmail.com>
2018-07-29 00:57:56 -0700

    add some more sections, clean up scrolling, organize and tweak css

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
+let navht = 0
+
 function resizeHeader() {
-    $("#nav-push").height($("#navbar").height());
+    let pusher = $('#nav-push')
+    let nav = $('#navbar')
+
+    navht = pusher.is(':visible') ? nav.height() : 0
+
+    pusher.height(navht);
 }
-$(document).ready(resizeHeader);
-$(window).resize(resizeHeader);
+
+function scroller() {
+    let tgt = $(this.hash)
+    let nav = $('#navbar')
+    $('html,body').animate({
+        scrollTop: tgt.position().top - navht
+    })
+    return false
+}
+
+$(() => {
+    resizeHeader();
+    $(window).resize(resizeHeader);
+    $('.nav-link').click(scroller);
+    $('.code-block, .nav-link').addClass('pop');
+})
